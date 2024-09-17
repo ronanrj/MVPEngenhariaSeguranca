7cd32c8a1ce69e249dab09d6f21c1300450a5ab1
Ronan Souza Pimenta <ronanrj@gmail.com>
2024-09-17 14:49:05 -0300

    feat: :sparkles: teste criado e passando

--- api/model/avaliador.py
+++ api/model/avaliador.py
@@ -3,11 +3,11 @@ from model.modelo import Model
 
 class Avaliador:
 
-    def avaliar(model, X_test, Y_test):
+    def avaliar(self,model, X_test, Y_test):
         """ Faz uma predição e avalia o modelo. Poderia parametrizar o tipo de
         avaliação, entre outros.
         """
-        predicoes = Model.preditor(model, X_test)
+        predicoes = Model.preditor(self,model, X_test)
         
         # Caso o seu problema tenha mais do que duas classes, altere o parâmetro average
         return accuracy_score(Y_test, predicoes)
--- api/test_modelos.py
+++ api/test_modelos.py
@@ -1,3 +1,5 @@
+# Importando as classes necessárias
+import pandas as pd  # Faltava a importação do pandas
 from model import *
 
 # To run: pytest -v test_modelos.py
@@ -8,62 +10,31 @@ modelo = Model()
 avaliador = Avaliador()
 
 # Parâmetros    
-url_dados = "./MachineLearning/data/test_dataset_diabetes.csv"
-colunas = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+url_dados_X = "./MachineLearning/data/X_test_dataset_heart.csv"
+url_dados_y = "./MachineLearning/data/y_test_dataset_heart.csv"  # Arquivo com a coluna de saída
+colunas = ['age', 'sex', 'cp', 'trtbps', 'chol', 'fbs', 'restecg', 'thalachh', 'exng', 'oldpeak']  # Incluindo apenas as colunas necessárias
 
-# Carga dos dados
-dataset = Carregador.carregar_dados(url_dados, colunas)
-array = dataset.values
-X = array[:,0:-1]
-y = array[:,-1]
-    
-# Método para testar o modelo de Regressão Logística a partir do arquivo correspondente
-# O nome do método a ser testado necessita começar com "test_"
-def test_modelo_lr():  
-    # Importando o modelo de regressão logística
-    lr_path = './MachineLearning/models/diabetes_lr.pkl'
-    modelo_lr = Model.carrega_modelo(lr_path)
+# Carga dos dados - Incluindo a coluna 'output'
+# Carga das features
+dataset = carregador.carregar_dados(url_dados_X, colunas)
+X = dataset.values  # Features
 
-    # Obtendo as métricas da Regressão Logística
-    acuracia_lr = Avaliador.avaliar(modelo_lr, X, y)
-    
-    # Testando as métricas da Regressão Logística 
-    # Modifique as métricas de acordo com seus requisitos
-    assert acuracia_lr >= 0.78 
-    # assert recall_lr >= 0.5 
-    # assert precisao_lr >= 0.5 
-    # assert f1_lr >= 0.5 
- 
-# Método para testar modelo KNN a partir do arquivo correspondente
-def test_modelo_knn():
-    # Importando modelo de KNN
-    knn_path = './MachineLearning/models/diabetes_knn.pkl'
-    modelo_knn = Model.carrega_modelo(knn_path)
+# Carga da coluna alvo
+y = pd.read_csv(url_dados_y).values.ravel()  # Carregar a coluna de saída separadamente
 
-    # Obtendo as métricas do KNN
-    acuracia_knn = Avaliador.avaliar(modelo_knn, X, y)
-    
-    # Testando as métricas do KNN
-    # Modifique as métricas de acordo com seus requisitos
-    assert acuracia_knn >= 0.78
-    # assert recall_knn >= 0.5 
-    # assert precisao_knn >= 0.5 
-    # assert f1_knn >= 0.5 
-    
-# Método para testar pipeline Random Forest a partir do arquivo correspondente
-def test_modelo_rf():
-    # Importando pipeline de Random Forest
-    rf_path = './MachineLearning/pipelines/rf_diabetes_pipeline.pkl'
-    modelo_rf = Pipeline.carrega_pipeline(rf_path)
 
-    # Obtendo as métricas do Random Forest
-    acuracia_rf = Avaliador.avaliar(modelo_rf, X, y)
-    
-    # Testando as métricas do Random Forest
-    # Modifique as métricas de acordo com seus requisitos
-    assert acuracia_rf >= 0.78
-    # assert recall_rf >= 0.5 
-    # assert precisao_rf >= 0.5 
-    # assert f1_rf >= 0.5
-    
+# Verificando o shape dos dados
+print(f"Shape de X: {X.shape}")  # Deve ser (n_samples, 10)
+print(f"Shape de y: {y.shape}")  # Deve ser (n_samples,)
 
+# Método para testar o modelo Naive Bayes a partir do arquivo correspondente
+def test_modelo_nb():
+    # Importando o pipeline salvo (que contém o scaler e o modelo)
+    pipeline_path = './MachineLearning/pipelines/nb_heart_pipeline.pkl'
+    modelo_pipeline = modelo.carrega_modelo(pipeline_path)
+
+    # Avaliando o pipeline diretamente com os dados de teste
+    acuracia_nb = avaliador.avaliar(modelo_pipeline, X, y)
+    
+    # Testando as métricas do Naive Bayes
+    assert acuracia_nb >= 0.78
